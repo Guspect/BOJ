007bdafd0e47cd57d93c187b785bd5952bf1ef4d
Guspect <58131026+Guspect@users.noreply.github.com>
2023-03-25 00:08:50 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = 0;
+        int B = 0;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        System.out.printf("%d",A+B);
+        
+    }
+}
