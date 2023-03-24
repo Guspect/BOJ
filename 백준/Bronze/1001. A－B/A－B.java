def2cc4e52ee440327c61ce628a86152d6e0ecd5
Guspect <58131026+Guspect@users.noreply.github.com>
2023-03-25 00:10:39 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner (System.in);
+        int A,B = 0;
+        A = s.nextInt();
+        B = s.nextInt();
+        System.out.printf("%d",A-B);
+    }
+}
