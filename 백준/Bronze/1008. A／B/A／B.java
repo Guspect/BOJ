d93bba7071d12e631a38d2f5a93008ce125b50c2
Guspect <58131026+Guspect@users.noreply.github.com>
2023-03-25 00:10:59 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        Double A,B = 0.0;
+        A = s.nextDouble();
+        B = s.nextDouble();
+        System.out.println(A/B);
+        }
+    }
+        
