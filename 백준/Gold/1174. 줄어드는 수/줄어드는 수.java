5f7d8932cd13755e0b22715afb8c010f94d30f8d
Guspect <58131026+Guspect@users.noreply.github.com>
2023-03-27 23:45:05 +0900

    [Gold V] Title: 줄어드는 수, Time: 136 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+	public static ArrayList<Long> list;
+	
+	public static void main(String args[])throws IOException {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));		
+		int n = Integer.parseInt(br.readLine());
+		list = new ArrayList<>();
+
+		if(n <= 10) {
+			System.out.println(n-1);
+		}else if(n > 1023) {
+			System.out.println("-1");
+		}else {
+			for(int i = 0; i < 10; i++) {
+				make_num(i, 1);
+			}
+			Collections.sort(list);
+			System.out.println(list.get(n-1));
+		}
+		
+	}
+	public static void make_num(long num, int len) {
+		if(len > 10) {
+			return;
+		}
+		
+		list.add(num);
+		for(int i = 0; i < num%10; i++) {
+			make_num((num*10) + i, len+1);
+		}
+	}
+	
+}
