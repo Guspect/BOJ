1cd060d4d58319eef8bf3e559068f30a5659bcab
Guspect <58131026+Guspect@users.noreply.github.com>
2024-03-11 18:32:05 +0900

    [Bronze II] Title: 공 바꾸기, Time: 140 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[])throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		int m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start_idx = Integer.parseInt(st.nextToken());
+			int end_idx = Integer.parseInt(st.nextToken());
+			start_idx--;
+			end_idx--;
+			
+			int temp = arr[start_idx];
+			arr[start_idx] = arr[end_idx];
+			arr[end_idx] = temp;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < arr.length; i++) {
+			sb.append(arr[i] + " ");
+		}
+		
+		System.out.println(sb);
+		
+	}
+}
+ 
